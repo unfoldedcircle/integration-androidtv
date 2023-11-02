aca4a4462144685ce7582fcd8f1bf359b874747a
Markus Zehnder <business@markuszehnder.ch>
2023-11-02 09:58:46 +0100

    reformatted apps.py with Black

--- intg-androidtv/apps.py
+++ intg-androidtv/apps.py
@@ -1,26 +1,10 @@
 Apps = {
-    'Youtube': {
-        'url': 'https://www.youtube.com'
-    },
-    'Prime Video': {
-        'url': 'https://app.primevideo.com'
-    },
-    'Plex': {
-        'url': 'plex://'
-    },
-    'Netflix': {
-        'url': 'netflix://'
-    },
-    'HBO Max': {
-        'url': 'https://play.hbomax.com'
-    },
-    'Emby': {
-        'url': 'embyatv://tv.emby.embyatv/startapp'
-    },
-    'Disney+': {
-        'url': 'https://www.disneyplus.com'
-    },
-    'Apple TV': {
-        'url': 'https://tv.apple.com'
-    }
+    "Youtube": {"url": "https://www.youtube.com"},
+    "Prime Video": {"url": "https://app.primevideo.com"},
+    "Plex": {"url": "plex://"},
+    "Netflix": {"url": "netflix://"},
+    "HBO Max": {"url": "https://play.hbomax.com"},
+    "Emby": {"url": "embyatv://tv.emby.embyatv/startapp"},
+    "Disney+": {"url": "https://www.disneyplus.com"},
+    "Apple TV": {"url": "https://tv.apple.com"},
 }
