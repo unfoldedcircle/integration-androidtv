c5681f3fab3411c8760f3cadad848b444790a47c
Lars Jansen <lars@ljpc.nl>
2024-01-08 16:15:53 +0100

    feat: added more apps (#21)

--- intg-androidtv/apps.py
+++ intg-androidtv/apps.py
@@ -9,6 +9,10 @@ Apps = {
     "Emby": {"url": "embyatv://tv.emby.embyatv/startapp"},
     "Disney+": {"url": "https://www.disneyplus.com"},
     "Apple TV": {"url": "https://tv.apple.com"},
+    "Spotify": {"url": "spotify://"},
+    "Ziggo": {"url": "ziggogo://"},
+    "Videoland": {"url": "videoland-v2://"},
+    "Steam Link": {"url": "steamlink://"},
 }
 
 # Direct application-id mappings to friendly names
@@ -33,16 +37,26 @@ IdMappings = {
     "com.google.android.videos": "Google TV",
     "tv.wuaki": "Rakuten TV",
     "homedia.sky.sport": "SKY",
+    "com.teamsmart.videomanager.tv": "SmartTube",
+    "nl.rtl.videoland.v2": "Videoland",
+    "com.disney.disneyplus": "Disney+",
+    "com.netflix.ninja": "Netflix",
+    "org.jellyfin.androidtv": "Jellyfin",
+    "com.discovery.dplay": "discovery+",
+    "com.talpa.kijk": "KIJK",
+    "nl.newfaithnetwork.app": "New Faith Network",
+    "nl.uitzendinggemist": "NPO Start",
+    "com.valvesoftware.steamlink": "Steam Link",
+    "org.videolan.vlc": "VLC",
+    "com.ziggo.tv": "Ziggo GO TV",
 }
 
 # Application-ID substring mappings to friendly names
 # Used to show which app is currently in the foreground (currently playing)
 NameMatching = {
-    "netflix": "Netflix",
     "youtube": "YouTube",
     "amazonvideo": "Prime Video",
     "hbomax": "HBO Max",
-    "disney": "Disney+",
     "apple": "Apple TV",
     "plex": "Plex",
     "kodi": "Kodi",
